5353d1cce6965019bf103a5960d5346cdd1c8ca8
Daniel Santander <danyvo1d@gmail.com>
2020-10-13 10:53:32 -0500

    Styled book panels

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,28 +3,49 @@ import PropTypes from 'prop-types';
 
 const Book = ({ book, remove }) => (
   <div className="book book-panel">
-    <div className="book-category">{ book.category }</div>
-    <div className="book-title">{ book.title }</div>
-    <div className="book-author">Author</div>
-    <div className="options">
-    <a className="item-option"
-        href="#"
+    <div className="book-info-container">
+      <div className="book-category">{ book.category }</div>
+      <div className="book-title">{ book.title }</div>
+      <div className="book-author">Author</div>
+      <div className="options">
+        <a className="item-option"
+            href="#"
+            >
+            Comments
+        </a>
+        <a className="item-option"
+            href="#"
+            onClick={() => {
+              remove(book, book.id);
+            }}
         >
-        Comments
-      </a>
-      <a className="item-option"
-        href="#"
-        onClick={() => {
-          remove(book, book.id);
-        }}
-      >
-        Delete
-      </a>
-      <a className="item-option"
-        href="#"
-      >
-        Edit
-      </a>
+            Delete
+        </a>
+        <a className="item-option"
+            href="#"
+        >
+          Edit
+        </a>
+      </div>
+    </div>
+
+    <div className="circle-container">
+      <div className="circle">
+      </div>
+      <div className="circle-data">
+        <span>64%</span>
+        <p>Completed</p>
+      </div>
+    </div>
+
+    <div className="chapter-container">
+      <h1 className="chapter">
+        CURRENT CHAPTER
+      </h1>
+      <p>Chapter 17</p>
+      <div className="progress-rectangle">
+        <span>UPDATE PROGRESS</span>
+      </div>
     </div>
   </div>
 );
